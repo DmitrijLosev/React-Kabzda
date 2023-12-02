d26299ea7fd7fdb21cbfd6251948910b4d7ae13d
DmitrijLosev <dmitrijslosevs@inbox.lv>
2023-12-02 11:03:53 +0200

    Story Book

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -3,7 +3,7 @@ import {RatingValueType} from "./ControlledRating";
 
 type RatingPropsType={
     defaultValue?:RatingValueType;
-    onChange:(value:RatingValueType)=>void
+    onChange?:(value:RatingValueType)=>void
 }
 
 
@@ -13,11 +13,11 @@ export function Rating(props:RatingPropsType) {
    const [rating, setRating] = useState<RatingValueType>(defaultValue!)
 
     return <div>
-        <Star selected={rating > 0} setNewRating={()=>{setRating(1); onChange(1)}}/>
-        <Star selected={rating > 1} setNewRating={()=>{setRating(2); onChange(2)}}/>
-        <Star selected={rating > 2} setNewRating={()=>{setRating(3); onChange(3)}}/>
-        <Star selected={rating > 3} setNewRating={()=>{setRating(4); onChange(4)}}/>
-        <Star selected={rating > 4} setNewRating={()=>{setRating(5); onChange(5)}}/>
+        <Star selected={rating > 0} setNewRating={()=>{setRating(1); onChange!(1)}}/>
+        <Star selected={rating > 1} setNewRating={()=>{setRating(2); onChange!(2)}}/>
+        <Star selected={rating > 2} setNewRating={()=>{setRating(3); onChange!(3)}}/>
+        <Star selected={rating > 3} setNewRating={()=>{setRating(4); onChange!(4)}}/>
+        <Star selected={rating > 4} setNewRating={()=>{setRating(5); onChange!(5)}}/>
     </div>
 }
 
